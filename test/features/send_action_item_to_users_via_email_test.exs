a452de8540027b84eedb0d87d811c3d08f18d35d
Travis Vander Hoop <vanderhoop@me.com>
2019-02-26 17:09:10 -0600

    Remove unit test assertion in e2e test

--- test/features/send_action_item_to_users_via_email_test.exs
+++ test/features/send_action_item_to_users_via_email_test.exs
@@ -10,17 +10,15 @@ defmodule SendActionItemToUsersViaEmailTest do
 
     @tag [
       idea: %Idea{category: "action-item", body: "Get better"},
-      idea_author: :facilitator,
       retro_stage: "action-items",
     ]
-    test "Distributing action items via email", ~M{retro, session: facilitator_session, facilitator} do
+    test "Distributing action items via email", ~M{retro, session: facilitator_session} do
       retro_path = "/retros/" <> retro.id
       facilitator_session = visit(facilitator_session, retro_path)
 
       click_and_confirm(facilitator_session, "Send Action Items")
 
       emails = Emails.action_items_email(retro.id)
-      assert emails.html_body =~ "Get better (#{facilitator.name})"
 
       emails |> assert_delivered_email
     end
