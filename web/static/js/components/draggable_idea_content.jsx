36e2139c8ee391d0c3fd7c5bb6b66892da435b9e
Travis Vander Hoop <vanderhoop@me.com>
2019-03-06 10:43:12 -0600

    WIP - implement draggability of Ideas using react-dnd
      - i need to figure out how to test the wrapped source
      - i need to figure out how to make category column a viable drag target
        - this drag target should only be active during the idea-generation stage

---
+++ web/static/js/components/draggable_idea_content.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { DragSource } from "react-dnd"
+import PropTypes from "prop-types"
+
+import IdeaContentBase from "./idea_content_base"
+
+import * as AppPropTypes from "../prop_types"
+
+// implement contract for react-dnd drag sources
+const dragSourceContract = {
+  beginDrag: props => {
+    return {
+      idea: props.idea,
+    }
+  },
+}
+
+// collects props as drag events begin and end
+const collect = (connect, monitor) => {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  }
+}
+
+const IdeaContentConnected = props => {
+  const { connectDragSource, ...rest } = props
+
+  return connectDragSource(
+    <div>
+      <IdeaContentBase {...rest} />
+    </div>
+  )
+}
+
+IdeaContentConnected.propTypes = {
+  idea: AppPropTypes.idea.isRequired,
+  retroChannel: AppPropTypes.retroChannel.isRequired,
+  currentUser: AppPropTypes.presence.isRequired,
+  stage: AppPropTypes.stage.isRequired,
+  assignee: AppPropTypes.presence,
+  canUserEditIdeaContents: PropTypes.bool.isRequired,
+  isTabletOrAbove: PropTypes.bool.isRequired,
+}
+
+IdeaContentConnected.defaultProps = {
+  assignee: null,
+}
+
+export default DragSource(
+  "IDEA",
+  dragSourceContract,
+  collect
+)(IdeaContentConnected)
