bfbf1c40c9f8851470ce1238864b8b465894eadb
Travis Vander Hoop <vanderhoop@me.com>
2019-03-05 19:39:42 -0600

    Lint

--- web/static/js/components/category_column.jsx
+++ web/static/js/components/category_column.jsx
@@ -33,9 +33,7 @@ export const CategoryColumn = props => {
         <p><strong>{category}</strong></p>
       </div>
       <div className={`ui fitted divider ${styles.divider}`} />
-      {!!ideas.length &&
-        <IdeaList {...props} />
-      }
+      {!!ideas.length && <IdeaList {...props} /> }
     </IdeaDropTarget>
   )
 }
