4eafd2c61509fa694be3ea2d43aa32f7e75dce84
Cody Duval <cody.duval@gmail.com>
2019-03-11 21:00:21 -0400

    Fix typo in .formatter.ex, eliminate calls to transport macro.

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,7 @@
 [
   import_deps: [:phoenix],
-  inputs: ["mix.exs", "*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"]
+  inputs: ["mix.exs", "*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"],
   line_length: 120,
   locals_without_parens: [],
-  trailing_comma: true,
+  trailing_comma: true
 ]
-
